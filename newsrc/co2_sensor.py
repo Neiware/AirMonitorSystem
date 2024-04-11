8604d196825e182e9cd4efeba9df913377ec1a02
Neiware <neidercm@hotmail.com>
2024-04-11 01:45:39 +0100

    Se determina co2 valor

--- newsrc/co2_sensor.py
+++ newsrc/co2_sensor.py
@@ -10,8 +10,8 @@ class Co2_sensor(Abc_sensor):
 		}
 	alive = False
 	pwm_pin = None
-	MIN_CYCLE_TIME = 1000
 	#set by datasheet
+	MIN_CYCLE_TIME = 1000
 	MAX_CYCLE_TIME = 1200
 
 	#get and setter
@@ -30,7 +30,8 @@ class Co2_sensor(Abc_sensor):
 	#co2 cannot be 0
 	def _set_data_co2(self, value):
 		if  value > 0 and value != self._data_co2():
-			self.data['Co2'] = value
+			self.data['Co2'] = round(value,2)
+
 			self.data['error'] = False
 		else:
 			self.data['Co2'] = 0
@@ -38,19 +39,21 @@ class Co2_sensor(Abc_sensor):
 
 
 	def timeMs(self):
-		return self.time.perf_counter_ns()/1000000
+		return time.perf_counter_ns()/1000000
 
 	def co2_ppm(self,time_high, time_low):
 		return 2000*((time_high - 2)/(time_high + time_low - 4))
+
 	#define which time state to obtain
 	# state = False (Low state)
 	# state = True (High state)
-	def _time_state(self, state):
+	def _state_time(self, state):
 		start_time = self.timeMs()
 		pwm_value = state
 		threshold_Ms = 1000
 		time_state_ms = 0
-		while(pwm_value == state or time_state_ms < threshold_Ms):
+
+		while(pwm_value == state and time_state_ms < threshold_Ms):
 			pwm_value = GPIO.input(self.pwm_pin)
 			#to prevent infinte loop, pwm reading cannot exced 1000 ms
 			time_state_ms = self.timeMs() - start_time
@@ -58,23 +61,26 @@ class Co2_sensor(Abc_sensor):
 
 	def read_data(self):
 		if self.alive is False:
-			print("dead")
 			return
 
 		cycle_time = 0
 		attepmts_reading = 0
 		while attepmts_reading < 5:
 
-			high_time = self._time_state(True)
-			low_time = self._time_state(False)
+			high_time = self._state_time(True)
+			low_time =  self._state_time(False)
 
 			cycle_time = high_time + low_time
+			attepmts_reading = attepmts_reading + 1
+			print(cycle_time)
+
 			if cycle_time > self.MAX_CYCLE_TIME or cycle_time < self.MIN_CYCLE_TIME:
 				self.data['error'] = True
 				continue
 			else:
 				co2_data = self.co2_ppm(high_time, low_time)
 				self._set_data_co2(co2_data)
+				break
 
 	def __init__ (self):
 		try:
